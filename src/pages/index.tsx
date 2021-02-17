c8828c86a963cb0c1043898956dc25d8c27c5087
Abhay <abhayonlyone@gmail.com>
2021-02-17 19:57:30 +0530

    fixed pixelated logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,29 +5,19 @@ import Img from "gatsby-image"
 
 import RandomShayari from "../components/random-shayari"
 
+import FeatherSvg from "../images/feather.svg"
+
 import "./index.css"
-import { graphql, useStaticQuery } from "gatsby"
 const IndexPage = () => {
-  const data = useStaticQuery(graphql`
-    query blackFeather {
-      file(relativePath: { eq: "feather-black.png" }) {
-        childImageSharp {
-          fixed(width: 64) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-    }
-  `)
   return (
     <div className="content">
       <SEO title="Home" />
 
       <h1>
-        <Img fixed={data.file.childImageSharp.fixed} />
+        <img src={FeatherSvg} style={{width: `64px`}} />
         <span style={{ marginLeft: `-1em` }}>Gulzar avatar</span>
       </h1>
-      <p style={{color: 'gray'}}>
+      <p style={{ color: "gray" }}>
         Welcome to Gulzar avatar. This site creates gulzar like Shayaris for
         you. We use the simplest algorithm, because that's all it takes. Click
         below to generate a psuedo-random generic shayari.
